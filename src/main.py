d5688b0c61f5571252aaa2fa0bcc625928efa85e
White Fox <2548708+HICWhiteFox@users.noreply.github.com>
2024-12-19 15:56:05 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,10 +14,14 @@ class ApiOutput(BaseModel):
 
 app = FastAPI() # creamos el api
 model = joblib.load("model.joblib") # cargamos el modelo.
+vectorizer = joblib.load("vectorizer.joblib")
+
 
 @app.post("/fake") # creamos api que permita requests de tipo post.
 async def define_sentiment(data: ApiInput) -> ApiOutput:
-    predictions = model.predict(data.texts).flatten().tolist() # generamos la predicción
+    # Convertir los textos en vectores
+    text_vectors = vectorizer.transform(data.texts)
+    predictions = model.predict(text_vectors).flatten().tolist() # generamos la predicción
     preds = ApiOutput(is_fake=predictions) # estructuramos la salida del API.
     return preds # retornamos los resultados
 
